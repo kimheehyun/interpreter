a10ba51665d99742d859a887e7710e78d5f8c270
heehyun <wibeau@gachon.ac.kr>
2025-04-02 17:49:26 +0900

    괴로웠다. (2) 이게 진짜임

--- interpreter/interpreter.cpp
+++ interpreter/interpreter.cpp
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 enum {
-  //ㅎㅇ
-	ERR=-1,
 
-	PLUS=1,
-	STAR=2,
-	NUMBER=3,
-	LP=4,
-	RP=5,
-	ID=6,
-	PRINT=7,
-	EQL=8,
+    ERR = -1,
 
-	//return
-	RET
-	
+    PLUS = 1,
+    STAR = 2,
+    NUMBER = 3,
+    LP = 4,
+    RP = 5,
+    ID = 6,
+    PRINT = 7,
+    EQL = 8,
+    ENT = 9,
+
+    //return
+    RET = 10
+
 } token;
 int num;
 int array[26];
@@ -26,188 +27,224 @@ int expr();
 int error();
 int factor();
 
-
 //이 array 정체는 알파벳 id 저장하는 칸이어씀 
-// 
-
+// 주어진 문법이 맞는지 아닌지.!!!!!!!!!!!!!!!!!!11 목적!!!!!!!!!11
 
 
 void get_token() {
-	/* next token --> token */
-	/* number value or id index --> num */
-	
-	int ch;
-	ch = getchar();
+    /* next token --> token */
+    /* number value or id index --> num */
 
-	//확인용
-	//printf("\ncheck:%c",ch);
+    int ch;
+    ch = getchar();
 
-
-	//PRINT
-	if (ch == 'p') {
-		get_token();
-		if(ch=='r')
-			get_token();
-		if (ch == 'i')
-			get_token();
-		if (ch == 'n')
-			get_token();
-		if (ch == 't')
-			token = PRINT;
-			return;
-			
-	}
+    //확인용
+    //printf("\ncheck:%c",ch);
 
 
 
-	else {
+    // 공백 재낌.
+    if (ch == ' ') {
+        ch = getchar();
 
-		//NUMBER
-		if (48 <= ch && ch <= 57) {
+    }
 
-			token = NUMBER;
-			num = ch;
-			return;
-		}
+    //PRINT
+    if (ch == 'p') {
+        ch = getchar();
 
-		//ID
-		else if ('a' <= ch && ch <= 'z') {
+        if (ch == 'r')
+            ch = getchar();
 
-			num = ch - 97;
-			token = ID;
-			get_token();
+        if (ch == 'i')
+            ch = getchar();
 
+        if (ch == 'n')
+            ch = getchar();
 
-		}
+        if (ch == 't')
+            token = PRINT;
+        ch = getchar();
+        return;
 
-		else {
-			switch (ch) {
-			case '+':
-				token = PLUS;
-				get_token();
-				break;
-			case '*':
-				token = STAR;
-				get_token();
-				break;
-			case '(':
-				token = LP;
-				get_token();
-				break;
-			case ')':
-				token = RP;
-				
-				break;
-			case '=':
-				token = EQL;
-				expr();
-				break;
-
-
-			default:
-
-				error();
-				break;
-
-
-			}
-			
-			return;
-
-		}
-
-	}
+    }
 
 
 
+    else {
 
+        //NUMBER 0~9
+        if (48 <= ch && ch <= 57) {
+
+            token = NUMBER;
+
+
+            num = ch - 48;
+
+
+            return;
+        }
+
+        //ID
+        else if ('a' <= ch && ch <= 'z') {
+
+            token = ID;
+
+
+            // num 에 알파벳 저장
+            num = ch - 97;
+            return;
+
+        }
+
+        else {
+            switch (ch) {
+            case '+':
+                token = PLUS;
+                break;
+            case '*':
+                token = STAR;
+                break;
+            case '(':
+                token = LP;
+                break;
+            case ')':
+                token = RP;
+
+                break;
+            case '=':
+                token = EQL;
+                break;
+
+            case '\n':
+                token = ENT;
+                break;
+
+
+            default:
+
+                error();
+                break;
+
+            }
+
+            return;
+
+        }
+
+    }
 
 
 }
 void statement() {
 
-	
-	if (token == ID) {
-		get_token();
-		if (token == EQL)
-			expr();
-	}
-
-	
-		///////////////////////////////////////////////
-
-	//print 를 친게 검출되면 expr 호출 
+    int r;
 
 
-	if (token == PRINT) {
-		expr();
-		printf("\n");
-
-	}
-
-	if (token == EQL) {
-		expr();
-
-	}
+    // ID=> =-> EXPR => /N 
+    if (token == ID) {
 
 
+        r = num;
+        get_token();
+        if (token == EQL) {
 
-	get_token();
+
+            get_token();
+
+            array[r] = num;
+
+            //확인용
+            //printf("r: %d\n",r);
+            //printf("array[r]:%d",array[r]);
+
+
+            //ID 다음 토큰이 EXR 인지
+            expr();
+
+
+        }
+
+        else
+        {
+            error();
+        }
+
+
+
+    }
+
+    ///////////////////////////////////////////////
+
+
+ // PRINT=> EXPR=> /N
+    else if (token == PRINT) {
+        get_token();
+        r = expr();
+        printf("> %d\n", r);
+    }
+
+    else {
+        error();
+    }
+
 
 }
 int expr() {
-	int r;
-	r = term();
-	while (token == PLUS) {
-		get_token();
-		r = r + term();
-	}
-	return (r);
+    int r;
+    r = term();
+    while (token == PLUS) {
+        get_token();
+        r = r + term();
+    }
+    return (r);
 }
 int term() {
-	int r;
-	r = factor();
-	while (token == STAR) {
-		get_token();
-		r = r * factor();
-	}
-	return (r);
+    int r;
+    r = factor();
+    while (token == STAR) {
+        get_token();
+        r = r * factor();
+    }
+    return (r);
 }
 
 
 int factor() {
-	int r;
-	if (token == NUMBER) {
-		r = num;
-		get_token();
-	}
-	else if (token == ID) {
-		r = array[num];
-		get_token();
-	}
-	else if (token == LP) {
-		get_token();
-		r = expr();
-		if (token == RP)
-			get_token();
-		else
-			error();
-	}
-	else
-		error();
-	return (r);
+    int r = 0;
+
+    if (token == NUMBER) {
+        r = num;
+        //확인용
+        //printf("rrrrrrrrrrrrr: %d\n", r);
+
+        get_token();
+    }
+    else if (token == ID) {
+        r = array[num];
+        get_token();
+    }
+    else if (token == LP) {
+        get_token();
+        r = expr();
+        if (token == RP)
+            get_token();
+        else
+            error();
+    }
+    else
+        error();
+    return (r);
 }
 void main() {
 
-	
-		get_token();
-		statement();
-	
+    while (1) {
+        get_token();
+        statement();
+    }
 }
 int error() {
 
-	printf("error!");
-	return -1;
-
-	
+    printf("SYNTAX error!\n");
+    return -1;
 }
